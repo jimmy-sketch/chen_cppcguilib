2cb74d35107dd6b49d3f2830a49be24a469e5ec7
jimmy-sketch <jimmy163mail@163.com>
2024-10-06 13:16:45 +0800

    完成多行文本相关方法

---
+++ chen_cppcguilib/multiLineText.cpp
@@ -0,0 +1,24 @@
+#include "multiLineText.h"
+
+int multiLineText::getWidth() const
+{
+    return calcuateLength();
+}
+
+int multiLineText::getHeight() const
+{
+    return textByLine.size();
+}
+
+void multiLineText::setMultiText(std::vector<std::string> textByLine)
+{
+    this->textByLine = textByLine;
+}
+
+size_t multiLineText::calcuateLength() const
+{
+    size_t maxLength = 0;
+    for (auto kv : textByLine)
+        maxLength = std::max(kv.size(), maxLength);
+    return maxLength;
+}
--- chen_cppcguilib/multiLineText.h
+++ chen_cppcguilib/multiLineText.h
@@ -3,6 +3,13 @@
 class multiLineText :
     public component
 {
-    
+public:
+    int getWidth() const override;
+    int getHeight() const override;
+    std::vector<std::string> getData() const override;
+    void setMultiText(std::vector<std::string> textByLine);
+private:
+    std::vector<std::string> textByLine;
+    size_t calcuateLength() const;
 };
 
