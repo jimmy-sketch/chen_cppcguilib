fd6e120ed6ee115617f7e2db9c7dedc2e2bdda89
dddungeon <nicedingzhen@outlook.com>
2024-10-19 16:19:45 +0800

    完善Pos

--- include/cgui/utils/pos.h
+++ include/cgui/utils/pos.h
@@ -18,5 +18,18 @@ struct Pos
     int row, col;
     int depth;
 };
+static bool operator<(Pos lhs, Pos rhs) {
+    // 深的反而小
+    if (lhs.depth < rhs.depth) {
+        return false;
+    }
+    else if (lhs.depth > rhs.depth) {
+        return true;
+    }
+    return (lhs.row < rhs.row) || (lhs.row == rhs.row && lhs.col < rhs.col);
+}
+static bool operator==(Pos lhs, Pos rhs) {
+    return (lhs.row == rhs.row) && (lhs.col == rhs.col) && (lhs.depth == rhs.depth);
+}
 
 }
